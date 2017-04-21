0701767c7f6ea008e422ad9e557655ce035128fe
Huang Chi-Han <huangchiheng@gmail.com>
2017-04-21 15:10:47 +0800

    Add migration and seeder.

---
+++ database/migrations/2017_04_19_041112_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+        * - id
+        * - api_token
+        * - name
+        * - timestamps
+        *
+        * @return void
+        */
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->string('api_token')->nullable();
+            $table->string('remember_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
